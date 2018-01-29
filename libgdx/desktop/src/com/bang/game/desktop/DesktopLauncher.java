87687fd5ae250e4d9c45704a53874156e7a9d967
aleeraser <alezini94@hotmail.it>
2018-01-29 11:46:43 +0100

    Added libgdx project files

---
+++ libgdx/desktop/src/com/bang/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bang.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bang.game.Bang;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bang(), config);
+	}
+}
