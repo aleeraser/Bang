db52e8369f029588b30ebd0e5d3ac25303be25b7
Carlo Stomeo <stomcarlo@gmail.com>
2018-01-29 14:33:30 +0100

    btn test

--- core/src/com/bang/game/Bang.java
+++ core/src/com/bang/game/Bang.java
@@ -11,10 +11,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
+import com.badlogic.gdx.graphics.GL20;
 
 public class Bang extends ApplicationAdapter {
   Stage stage;
-  TextButton button;
+  TextButton button, button2;
   TextButtonStyle textButtonStyle;
   BitmapFont font;
   Skin skin;
@@ -34,8 +35,12 @@ public class Bang extends ApplicationAdapter {
     textButtonStyle.down = skin.getDrawable("default-rect-down");
     //textButtonStyle.checked = skin.getDrawable("checked-button");
     button = new TextButton("Button1", textButtonStyle);
+    button2 = new TextButton("Button2", textButtonStyle);
     stage.addActor(button);
+    stage.addActor(button2);
     button.setSize(200, 80);
+    button2.setSize(200, 80);
+    button2.setPosition(stage.getWidth()-200, 0);
 
     button.addListener(new ChangeListener() {
 			
@@ -43,12 +48,23 @@ public class Bang extends ApplicationAdapter {
        public void changed(ChangeEvent event, Actor actor) {
 		       System.out.println("Click");				
 		   }
-		});
+    });
+    
+    button2.addListener(new ChangeListener() {
+
+      @Override
+      public void changed(ChangeEvent event, Actor actor) {
+        System.out.println("Click2");
+      }
+    });
 	}
 
 	@Override
 	public void render () {
-		super.render();
+    Gdx.gl.glClearColor(1, 1, 1, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    super.render();
     stage.draw();
 	}
 	
