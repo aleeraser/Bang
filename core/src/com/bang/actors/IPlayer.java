6671c58b2fbe909236fae1aadb7fa3a8e7702aaa
Carlo Stomeo <stomcarlo@gmail.com>
2018-02-07 17:38:57 +0100

    Improved Player and added Player interface

---
+++ core/src/com/bang/actors/IPlayer.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IPlayer extends Remote {
+
+    //void setPlayerList(ArrayList<Player> pl); //assumiamo che la lista venga inizializzata alla creazione della stanza e passata ad ogni giocatore.
+
+    void setIpList(ArrayList<String> ips) throws RemoteException; //assumiamo che la lista venga inizializzata alla creazione della stanza e passata ad ogni giocatore.
+
+    String getIp() throws RemoteException;
+
+    int getLifes() throws RemoteException;
+
+    int getDistancs() throws RemoteException;
+
+    void shot(String ip) throws RemoteException;
+
+    void decreaseLifes() throws RemoteException;
+
+}
--- core/src/com/bang/actors/Player.java
+++ core/src/com/bang/actors/Player.java
@@ -1,5 +1,3 @@
-package com.bang.actors;
-
 import java.net.MalformedURLException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
@@ -8,12 +6,11 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
 
-public class Player extends UnicastRemoteObject {
+public class Player extends UnicastRemoteObject implements IPlayer {
     private int lifes;
     private String ip;
     //private ArrayList<Card> handCards = new ArrayList<Card>();
@@ -22,6 +19,7 @@ public class Player extends UnicastRemoteObject {
     private int view; //ditanza a cui può sparare
     private int distance; //incremento della distanza a cui viene visto
     private ArrayList<Player> players = new ArrayList<Player>();
+    private ArrayList<String> ips = new ArrayList<String>(); //valutare se tenere la lista di ip o di player
 
     public Player() throws RemoteException {
         /*this.CharacterPower = genCharacter();
@@ -33,6 +31,10 @@ public class Player extends UnicastRemoteObject {
 
     public void setPlayerList(ArrayList<Player> pl) { //assumiamo che la lista venga inizializzata alla creazione della stanza e passata ad ogni giocatore.
         this.players = pl;
+    }
+
+    public void setIpList(ArrayList<String> ips) { //assumiamo che la lista venga inizializzata alla creazione della stanza e passata ad ogni giocatore.
+        this.ips = ips;
     }
 
     public String getIp() {
@@ -47,7 +49,7 @@ public class Player extends UnicastRemoteObject {
         return this.distance;
     }
 
-    public void shot(Player p) {
+    public void shot(String ip) {
         //todo implementare controllo distanza tra i e j 
 
         /* try {
@@ -60,7 +62,18 @@ public class Player extends UnicastRemoteObject {
             e.printStackTrace();
         } */
 
-        p.decreaseLifes();
+        try {
+            IPlayer target = (IPlayer) Naming.lookup("rmi://" + ip + "/Player");
+            target.decreaseLifes();
+            System.out.println(target.getLifes());
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void decreaseLifes() {
@@ -68,20 +81,24 @@ public class Player extends UnicastRemoteObject {
         if (this.lifes <= 0) {
             System.out.println("SONO MORTO"); //todo chiamare routine per aggiornare le liste dei player
         }
-
     }
 
-    private String findIp() {
+    private static String findIp() {
         SocketException exception = null;
 
         try {
             Enumeration e = NetworkInterface.getNetworkInterfaces();
+            int count = 0;
             while (e.hasMoreElements()) {
                 NetworkInterface n = (NetworkInterface) e.nextElement();
                 Enumeration ee = n.getInetAddresses();
                 while (ee.hasMoreElements()) {
                     InetAddress i = (InetAddress) ee.nextElement();
-                    return (i.getHostAddress());
+                    count++;
+                    if (count == 2) {
+                        System.out.println(i.getHostAddress());
+                        return (i.getHostAddress());
+                    }
                 }
             }
         } catch (SocketException e) {
@@ -90,5 +107,41 @@ public class Player extends UnicastRemoteObject {
         }
 
         return exception.toString();
+    }
+
+    private Boolean ping(String ip) {
+        try {
+            InetAddress inet = InetAddress.getByName(ip);
+            System.out.println("Sending Ping Request to " + ip);
+            if (inet.isReachable(5000)) {
+                System.out.println(ip + " is reachable.");
+                return true;
+            } else {
+                System.out.println(ip + "is NOT reachable.");
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("Exception:" + e.getMessage());
+            return false; //todo rilanciare l'eccezione a livello superiore
+        }
+    }
+
+    public static void main(String[] args) {
+        System.setProperty("java.rmi.server.hostname", findIp());
+
+        try {
+            IPlayer server = new Player();
+            Naming.rebind("//" + server.getIp() + "/Player", server);
+
+            if (server.getIp() == "192.168.1.4") {
+                server.shot("192.168.1.6");
+            }
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
