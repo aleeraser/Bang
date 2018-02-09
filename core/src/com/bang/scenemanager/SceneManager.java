279a131940fb6948e6795a81363d9efa4a671dfe
aleeraser <alezini94@hotmail.it>
2018-02-09 12:01:29 +0100

    Changed skin and added adaptive button size.

--- core/src/com/bang/scenemanager/SceneManager.java
+++ core/src/com/bang/scenemanager/SceneManager.java
@@ -14,67 +14,72 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
 public class SceneManager {
 
-	protected GameScene gameScene;
-	protected BitmapFont font;
-	protected Skin skin;
-	protected TextureAtlas textureAtlas;
-	protected Batch batch;
+    protected GameScene gameScene;
+    protected BitmapFont font;
+    protected Skin skin;
+    protected TextureAtlas textureAtlas;
+    protected Batch batch;
     protected TextButtonStyle textButtonStyle;
     protected LabelStyle textStyle;
-	
-	public SceneManager() {
-		gameScene = null;
-		
-		// skinName = "default" o "visui" (o altre, se verranno aggiunte)
-        String skinPath, skinBtn, skinName = "visui";
-        if (skinName == "visui") {
-            skinPath = "skins/visui/uiskin";
-            skinBtn = "button";
-        } else {
+
+    public SceneManager() {
+        gameScene = null;
+
+        // skinName = "default" o "visui" (o altre, se verranno aggiunte)
+        String skinPath, skinBtn, skinName = "rusty-robot";
+
+        if (skinName == "default") {
             skinPath = "skins/default/uiskin";
             skinBtn = "default-rect";
+        } else if (skinName == "rusty-robot") {
+            skinPath = "skins/rusty-robot/rusty-robot-ui";
+            skinBtn = "button";
+        } else {
+            skinPath = "skins/visui/uiskin";
+            skinBtn = "button";
         }
-        
+
         font = new BitmapFont();
         textureAtlas = new TextureAtlas(Gdx.files.internal(skinPath + ".atlas"));
         skin = new Skin(Gdx.files.internal(skinPath + ".json"), textureAtlas);
-        
+
         // Button textures
         textButtonStyle = new TextButtonStyle();
         textButtonStyle.font = font;
         textButtonStyle.up = skin.getDrawable(skinBtn);
-        textButtonStyle.down = skin.getDrawable(skinBtn + "-down");
-        
+        textButtonStyle.down = skin.getDrawable(skinBtn + "-pressed");
+
         // Label textures
         textStyle = new LabelStyle();
         textStyle.font = font;
         textStyle.background = skin.getDrawable("textfield");
-	}
-	
-	public void setScene(GameScene scene) {
-		if (this.gameScene != null) this.gameScene.getStage().dispose();
-		this.gameScene = scene;		
-		//scene.setGameManager(this);
-		Gdx.input.setInputProcessor(scene.getStage());
-	}
+    }
 
-	public Stage getCurrentStage() {
-		return gameScene.getStage();
-	}
-	
-	public GameScene getCurrentScene() {
-		return gameScene;
-	}
-	
-	public Texture getCurrentBackgroundImage() {
-		return gameScene.getBackgroundImage();
-	}
-	
-	public Skin getSkin() {
-		return skin;
-	}
-	
-	public TextButtonStyle getTextButtonStyle() {
-		return textButtonStyle;
-	}
+    public void setScene(GameScene scene) {
+        if (this.gameScene != null)
+            this.gameScene.getStage().dispose();
+        this.gameScene = scene;
+        //scene.setGameManager(this);
+        Gdx.input.setInputProcessor(scene.getStage());
+    }
+
+    public Stage getCurrentStage() {
+        return gameScene.getStage();
+    }
+
+    public GameScene getCurrentScene() {
+        return gameScene;
+    }
+
+    public Texture getCurrentBackgroundImage() {
+        return gameScene.getBackgroundImage();
+    }
+
+    public Skin getSkin() {
+        return skin;
+    }
+
+    public TextButtonStyle getTextButtonStyle() {
+        return textButtonStyle;
+    }
 }
