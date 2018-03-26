70d2d3c269581aea9a858658101ce5502ed8db1f
Filippo Morselli <exmorse@gmail.com>
2018-03-26 09:41:29 +0200

    Show card in dialog

--- core/src/com/bang/scenemanager/SelectCardDialog.java
+++ core/src/com/bang/scenemanager/SelectCardDialog.java
@@ -3,12 +3,16 @@ package com.bang.scenemanager;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.bang.actors.Card;
 import com.bang.actors.IPlayer;
-import com.bang.actors.Player;
-import com.bang.gameui.OtherBoardGroup;
+import com.bang.utils.CardsUtils;
 
 public class SelectCardDialog extends Dialog {
 
@@ -41,11 +45,14 @@ public class SelectCardDialog extends Dialog {
 		
 		for(int i = 0; i < handCards.size(); i++) {
 			/* Carte in mano hanno indice <i + numero di carte in tavolo> */
-			this.button("Mano " + (i + 1), tableCards.size() + i);		
+			this.button("Mano " + (i + 1), tableCards.size() + i);
 		}
 		
 		for(int i = 0; i < tableCards.size(); i++) {
-			this.button(tableCards.get(i).getName(), i);		
+			//this.button(tableCards.get(i).getName(), i);
+			Drawable drawable = new TextureRegionDrawable(new TextureRegion(CardsUtils.getCardTexture(tableCards.get(i))));
+			ImageButton b = new ImageButton(drawable);
+			this.button(b, i);
 		}
 	}
 	
