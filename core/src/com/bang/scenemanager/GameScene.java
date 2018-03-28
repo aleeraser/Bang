3e2f0aec0795a8c4673698acc881141b84758c7a
Filippo Morselli <exmorse@gmail.com>
2018-03-28 13:12:35 +0200

    Duel

--- core/src/com/bang/scenemanager/GameScene.java
+++ core/src/com/bang/scenemanager/GameScene.java
@@ -533,7 +533,7 @@ public class GameScene extends Scene {
 					IPlayer enemy = players.get(enemyIndex);
 					
 					enemy.duello(res, res, me.getPos(new int[players.size()]), new int[players.size()]);
-				
+					me.duello(true, false, enemyIndex, new int[players.size()]);
 					if (!res) {
 						me.duello(false, false, -1, new int[players.size()]);
 						me.decreaselives(new int[players.size()]);
