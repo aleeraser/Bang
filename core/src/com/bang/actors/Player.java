c0862902d3e12e196f6ce41a24fc6a87c5c5e16c
Carlo Stomeo <stomcarlo@gmail.com>
2018-04-04 12:48:43 +0200

    fault tolerance in duello

--- core/src/com/bang/actors/Player.java
+++ core/src/com/bang/actors/Player.java
@@ -373,10 +373,15 @@ public class Player extends UnicastRemoteObject implements IPlayer {
                 }
                 if (this.duel && !this.duelTurn ){
                     try{
-                        this.clock.clockIncreaseLocal();
-                        players.get(this.duelEnemy).getPos(this.clock.getVec());
-                        this.startTimeoutTime = System.currentTimeMillis();
-                        //this code is executed only if the player is still up
+                        if( players.get(this.duelEnemy) != null){
+                            this.clock.clockIncreaseLocal();
+                            players.get(this.duelEnemy).getPos(this.clock.getVec());
+                            this.startTimeoutTime = System.currentTimeMillis();
+                            //this code is executed only if the player is still up
+                        }
+                        else{
+                            throw( new RemoteException());
+                        }
                     } catch (RemoteException e) {
                         this.duello(false, false,-1,this.clock.getVec());
                     }
