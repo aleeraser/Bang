0ca3824ef03a15b64cc739d801e77cc869817a12
Carlo Stomeo <stomcarlo@gmail.com>
2018-04-05 13:19:11 +0200

    debug

--- core/src/com/bang/game/Bang.java
+++ core/src/com/bang/game/Bang.java
@@ -87,7 +87,7 @@ public class Bang extends ApplicationAdapter {
                     gs.dismissDuelDialog();
                 }
 
-                if (!me.isInBangTurn().matches("") && me.shouldUpdateDuel()) {
+                if (!me.isInBangTurn().matches("") ) {
                     System.out.println("Showing bang dialog");
                     System.out.println("IS MY TURN: " + me.isInBangTurn());
                     gs.showBangDialog(me.isInBangTurn(), me.getPlayers().get(me.getBangEnemy()).getCharacter().getName());
