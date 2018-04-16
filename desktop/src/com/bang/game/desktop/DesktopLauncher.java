e790c6ae359a6e6c67bb78806e0915bc73ada170
Carlo Stomeo <stomcarlo@gmail.com>
2018-04-16 13:06:00 +0200

    game closes on exception

--- desktop/src/com/bang/game/desktop/DesktopLauncher.java
+++ desktop/src/com/bang/game/desktop/DesktopLauncher.java
@@ -4,6 +4,7 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
+import com.badlogic.gdx.Gdx;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
@@ -11,13 +12,18 @@ import com.bang.game.Bang;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        System.setProperty("java.rmi.server.hostname", getPriorityIP());
-        System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
-		config.height = 700;
-		config.width = 1200;
-		config.resizable = false;
-		new LwjglApplication(new Bang(), config);
+        try{
+            LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+            System.setProperty("java.rmi.server.hostname", getPriorityIP());
+            System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
+            config.height = 700;
+            config.width = 1200;
+            config.resizable = false;
+            new LwjglApplication(new Bang(), config);
+        }catch (Exception e) {
+            e.printStackTrace();
+            Gdx.app.exit();
+        }
 	}
 	
 	public static String getPriorityIP() {
